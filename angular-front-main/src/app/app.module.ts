4d0b271b1b12a67f841d3d859135de17e7f14cc9
sebasmanu14 <sas.manobanda@yavirac.edu.ec>
2022-07-11 11:12:32 -0500

    app module error

--- angular-front-main/src/app/app.module.ts
+++ angular-front-main/src/app/app.module.ts
@@ -5,27 +5,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CareersComponent } from './careers/careers.component';
-import { ProductsComponent } from './products/products.component';
-<<<<<<< HEAD
-import { HttpClient } from '@angular/common/http';
-import { LevelsComponent } from './levels/levels.component';
 import { InstitutionalTutorsComponent } from './institutional-tutors/institutional-tutors.component';
-=======
+import { LevelsComponent } from './levels/levels.component';
+import { ProductsComponent } from './products/products.component';
 import { SchoolPeriodsComponent } from './school-periods/school-periods.component';
 import { StudentsComponent } from './students/students.component';
->>>>>>> 3e6522422fc4d4c0f9c4e14f8c79f4287afb71d8
 @NgModule({
   declarations: [
     AppComponent,
        ProductsComponent,
-<<<<<<< HEAD
        LevelsComponent,
-       InstitutionalTutorsComponent
-=======
+       InstitutionalTutorsComponent,
        SchoolPeriodsComponent,
        CareersComponent,
        StudentsComponent
->>>>>>> 3e6522422fc4d4c0f9c4e14f8c79f4287afb71d8
   ],
   imports: [
     BrowserModule,
